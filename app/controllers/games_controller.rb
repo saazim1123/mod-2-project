154f41a2734bd7c2d19cacf85d2fc4119dfbf66a
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-19 17:40:13 +0100

    Unique Genre drop down menu

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,19 +6,14 @@ class GamesController < ApplicationController
             @games = Game.all
         else
             @genre_id = Genre.find_by(name: params[:genre])
-            @games = Game.where(id: @genre_id.game_id)
+            @games = Game.all.select{|g| g.genres[0].name == @genre_id.name}
         end
     end
 
     def show
         
     end
-
-    def new
-        @game = current_user.games.build
-        @genres = Genre.all
-    end
-
+    
     private
         def game_params
             params.require(:game).permit(:title, :genre_id)
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,7 @@
 class Genre < ApplicationRecord
     belongs_to :game
+
+    def self.unique_genres
+        Genre.all.uniq{|genre| genre.name}
+    end
 end
