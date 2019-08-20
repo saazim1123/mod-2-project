ffb441c203a68e3d91da0d5948776474698ec0f5
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-20 14:19:26 +0100

    almost MVP

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
-    has_many :platforms
+    has_many :game_platforms
+    has_many :platforms, through: :game_platforms
     has_many :genres
     has_many :reviews
     has_many :users, through: :reviews
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_155251) do
+ActiveRecord::Schema.define(version: 2019_08_20_122304) do
+
+  create_table "game_platforms", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "platform_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -23,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_08_19_155251) do
 
   create_table "platforms", force: :cascade do |t|
     t.integer "game_id"
-    t.string "platforms"
+    t.string "name"
   end
 
   create_table "reviews", force: :cascade do |t|
