01fd8898b92b86955a0d41061787c5e7e07e420e
saazim1123 <saazim1123@gmail.com>
2019-08-20 14:13:32 +0100

    Review user bugs sorted and platform drop down created

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,6 +1,9 @@
 class Platform < ApplicationRecord
     belongs_to :game
 
+    def self.platforms
+        
+    end
 
     def self.unique_platforms
         Platform.all.uniq{|platform| platform.platforms}
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,46 @@ CSV.foreach('./lib/seeds/games_2018.csv', headers: true) do |row|
     games_2018 << row.to_h
 end
 
+games_2018.each do |game|
+    games_2018_titles << game["Title"]
+    games_2018_platforms << game["Platform(s)"]
+    games_2018_genres << game["Genre(s)"]
+end
+
+games_2018_titles.each do |title|
+    Game.create(title: title)
+end
 
 i = 1
-games_2018.each do |game| 
-    Game.create(title: game["Title"])
-    Platform.create(game_id: i, platforms: game["Platform(s)"])
-    Genre.create(game_id: i, name: game["Genre(s)"])
+games_2018_platforms.each do |platform| 
+    Platform.create(game_id: Game.find(i).id, platforms: platform.split(", "))
+    i += 1
+   
+end
+# games_2018_platforms.each do |platform| 
+#     Platform.create(game_id: Game.find(i).id, platforms: platform)
+#     i += 1
+# end
+
+i = 1
+games_2018_genres.each do |genre|
+    Genre.create(game_id: Game.find(i).id, name: genre)
     i += 1
 end
+
+# games_2018_genres.each do |genre|
+#     Game.all.each do |game|
+#         Genre.create(game_id: game.id, name: genre)
+#     end
+# end
+
+# i = 1
+# games_2018.each do |game| 
+#     Game.create(title: game["Title"])
+#     Platform.create(game_id: i, platforms: game["Platform(s)"])
+#     Genre.create(game_id: i, name: game["Genre(s)"])
+#     i += 1
+# end
 # games_2018.each{|game| games_2018_platforms << game["Platform(s)"]}
 # games_2018.each{|game| games_2018_genres << game["Genre(s)"]}
 
