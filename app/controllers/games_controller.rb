c1979a18638508de74c301195b11497514c5ff50
saazim1123 <saazim1123@gmail.com>
2019-08-21 10:05:33 +0100

    Yet to add search bad

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,16 +2,21 @@ class GamesController < ApplicationController
     before_action :find_game, only: [:show]
 
     def index
-        if params[:genre].blank?
+        if params[:genre].blank? && params[:platform].blank?
             @games = Game.all
-        else
-            @genre_id = Genre.find_by(name: params[:genre])
-            @games = Game.all.select{|g| g.genres[0].name == @genre_id.name}
+        elsif params[:platform].blank?
+            @games = Genre.find_by(name: params[:genre]).games
+        elsif params[:genre].blank? 
+            @games = Platform.find_by(name: params[:platform]).games
         end
     end
 
     def show
-        
+        if @game.reviews.blank?
+            @average_review = "No Reviews have been added to this game"
+        else
+            @average_review = @game.reviews.average(:rating).round(2)
+        end
     end
 
     private
